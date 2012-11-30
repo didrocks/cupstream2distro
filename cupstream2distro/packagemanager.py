cbd82bf5a032191b0cd018ad4b691fe3bbff89c2
Didier Roche <didier.roche@canonical.com>
2012-11-30 17:21:43 +0100

    hem hem, use the right name

--- cupstream2distro/packagemanager.py
+++ cupstream2distro/packagemanager.py
@@ -224,7 +224,7 @@ def upload_package(source, version, ppa):
     '''Upload the new package to a ppa'''
     # remove epoch is there is one
     version_for_source_file = version.split(':')[-1]
-    if subprocess.call(["dput", "ppa:{}".format(ppa), "{}_{}_source.changes".format(version_for_source_file, version)]) != 0:
+    if subprocess.call(["dput", "ppa:{}".format(ppa), "{}_{}_source.changes".format(source, version_for_source_file)]) != 0:
         raise Exception("The above command returned an error.")
 
 
