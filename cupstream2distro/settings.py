ce86d1bff390e400417b503397b485ab35e3034a
Didier Roche <didier.roche@canonical.com>
2014-01-03 14:10:11 +0100

    add support for artefacts and out rsync dir

--- cupstream2distro/settings.py
+++ cupstream2distro/settings.py
@@ -74,6 +74,7 @@ STACK_STATUS_PUBLISHING_DIR = "/iSCSI/jenkins/cu2d/result_publishing"
 SILO_CONFIG_FILENAME = "config"
 SILO_BUILDPPA_SCHEME = "didrocks/{}"
 SILO_DIR = os.path.expanduser("~/silos")
+SILO_RSYNCDIR = os.path.expanduser("~/rsync")
 CITRAIN_BINDIR = os.path.expanduser("~/citrain/citrain")
 (SILO_EMPTY, SILO_BUILTCHECKED, SILO_PUBLISHED, SILO_DONE) = range(4)
 
