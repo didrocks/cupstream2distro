e6af372c5dbdd0f9d2ac864b910bc895e9069df9
Didier Roche <didier.roche@canonical.com>
2014-01-06 14:03:43 +0100

    don't use 0, 0 is for loosers apparently

--- cupstream2distro/settings.py
+++ cupstream2distro/settings.py
@@ -71,6 +71,8 @@ STACK_STATUS_PUBLISHING_DIR = "/iSCSI/jenkins/cu2d/result_publishing"
 # for citrain
 SILO_NAME_LIST = []
 for i in xrange(11):
+    if i == 0:
+        continue # asac doesn't want to be 0 :p
     SILO_NAME_LIST.append("landing-{:03d}".format(i))
 SILO_CONFIG_FILENAME = "config"
 SILO_BUILDPPA_SCHEME = "ci-train-ppa-service/{}"
