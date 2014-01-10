ea40c7494978c8aa7206de6cc0bee69637393ee5
Didier Roche <didier.roche@canonical.com>
2014-01-10 09:31:21 +0100

    remove unused plugin and set the right number of silos

--- cupstream2distro/settings.py
+++ cupstream2distro/settings.py
@@ -70,7 +70,7 @@ STACK_STATUS_PUBLISHING_DIR = "/iSCSI/jenkins/cu2d/result_publishing"
 
 # for citrain
 SILO_NAME_LIST = []
-for i in xrange(1, 10):
+for i in xrange(1, 11):
     SILO_NAME_LIST.append("landing-{:03d}".format(i))
 SILO_CONFIG_FILENAME = "config"
 SILO_BUILDPPA_SCHEME = "ci-train-ppa-service/{}"
