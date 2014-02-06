16656f06ec49a4fa22a2f2bfd353d1d27f1f7ec8
Didier Roche <didier.roche@canonical.com>
2014-02-06 10:30:00 +0000

    add 10 silos

--- cupstream2distro/settings.py
+++ cupstream2distro/settings.py
@@ -74,7 +74,7 @@ STACK_STATUS_PUBLISHING_DIR = "/iSCSI/jenkins/cu2d/result_publishing"
 
 # for citrain
 SILO_NAME_LIST = []
-for i in xrange(1, 11):
+for i in xrange(1, 21):
     SILO_NAME_LIST.append("landing-{:03d}".format(i))
 SILO_CONFIG_FILENAME = "config"
 SILO_BUILDPPA_SCHEME = "ci-train-ppa-service/{}"
