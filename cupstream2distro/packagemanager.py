6ac14384ccb57e1d31b82712b65a4697522d7a38
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2014-07-31 14:49:10 +0200

    Try working around dogfood being slow by increasing the httplib timeout

--- cupstream2distro/packagemanager.py
+++ cupstream2distro/packagemanager.py
@@ -28,6 +28,7 @@ import shutil
 import sys
 import subprocess
 import urllib
+import socket
 
 import launchpadmanager
 import settings
@@ -186,6 +187,10 @@ def get_source_package_from_dest(source_package_name, dest_archive, dest_current
         os.makedirs(source_package_download_dir)
     os.chdir(source_package_download_dir)
 
+    # XXX: Temporary
+    timeout = socket.gettimeout()
+    socket.settimeout(300)
+
     try:
         sourcepkg = sort_by_date_created(dest_archive.getPublishedSources(exact_match=True, source_name=source_package_name, distro_series=series, version=dest_current_version))[0]
     except IndexError:
@@ -197,6 +202,8 @@ def get_source_package_from_dest(source_package_name, dest_archive, dest_current
     (stdout, stderr) = instance.communicate()
     if instance.returncode != 0:
         raise Exception(stderr.decode("utf-8").strip())
+
+    socket.settimeout(timeout)
 
     # check the dir exist
     splitted_version = dest_current_version.split(':')[-1].split('-')  # remove epoch is there is one
