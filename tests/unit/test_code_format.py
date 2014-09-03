a8288fb053501b12e3455740480cf75a5c1ae1ed
Robert Bruce Park <robert.park@canonical.com>
2014-09-03 15:43:38 -0700

    Enforce pep8 style in the unit tests.

---
+++ tests/unit/test_code_format.py
@@ -0,0 +1,16 @@
+import os
+
+import pep8
+import unittest
+
+traindir = os.path.join('..', 'citrain')
+
+class Pep8ConformanceTestCase(unittest.TestCase):
+    def test_pep8_conformance(self):
+        pep8style = pep8.StyleGuide(show_source=True)
+        pep8style.input_dir('..')
+        for filename in os.listdir(traindir):
+            filepath = os.path.join(traindir, filename)
+            if os.path.isfile(filepath):
+                pep8style.check_files([filepath])
+        self.assertEqual(pep8style.options.report.total_errors, 0)
